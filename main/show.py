e2756c0f520978a4c08dd907c1312552bd9c1b7f
PKC278 <973134713@qq.com>
2023-12-16 00:25:21 +0800

    优化

--- main/show.py
+++ main/show.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QBrush, QColor
 
 
 class TooltipListWidget(QtWidgets.QListWidget):
@@ -10,24 +11,29 @@ class TooltipListWidget(QtWidgets.QListWidget):
             QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint
         )
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
-
         # 获取屏幕尺寸并设置窗口位置和大小
         screen = QtWidgets.QDesktopWidget().screenGeometry()
-        self.setGeometry(screen.width() - 450, (screen.height() - 600) // 2, 400, 600)
+        self.setGeometry(screen.width() - 450, (screen.height() - 325) // 2, 400, 325)
 
         self.installEventFilter(self)  # 安装事件过滤器
 
         # 添加关闭按钮
-        self.close_button = QtWidgets.QPushButton("关闭", self)
-        self.close_button.clicked.connect(self.close)  # 修改为退出程序
-        self.close_button.move(335, -5)  # 将按钮移动到右上角
-        self.close_button.setFixedHeight(50)  # 设置按钮的高度
-        self.close_button.setFixedWidth(70)  # 设置按钮的宽度
-        self.close_button.setStyleSheet("background-color: rgba(255, 255, 255, 0);")
+        # self.close_button = QtWidgets.QPushButton("关闭", self)
+        # self.close_button.clicked.connect(self.close)  # 修改为退出程序
+        # self.close_button.move(335, -5)  # 将按钮移动到右上角
+        # self.close_button.setFixedHeight(50)  # 设置按钮的高度
+        # self.close_button.setFixedWidth(70)  # 设置按钮的宽度
+        # self.close_button.setStyleSheet("background-color: rgba(255, 255, 255, 0);")
 
     def add_item_with_tooltip(self, text):
         item = QtWidgets.QListWidgetItem()
         self.addItem(item)
+        # 设置项目的底色
+        if self.count() % 2 == 0:
+            item.setBackground(QBrush(QColor(240, 240, 240)))  # 浅灰色
+        else:
+            item.setBackground(QBrush(QColor(255, 255, 255)))  # 白色
+
         item.setSizeHint(QtCore.QSize(0, 65))  # 设置项目的大小
         lines = text.split("\n")  # 将文本分割为多行
         label_text = "\n".join(lines[:2])  # 只取前两行
@@ -51,8 +57,12 @@ class TooltipListWidget(QtWidgets.QListWidget):
     def exit_app(self):
         sys.exit()  # 添加一个方法来退出程序
 
+    def mousePressEvent(self, event):
+        # 无论鼠标点击哪里，都直接关闭窗口
+        self.close()
+
     def keyPressEvent(self, event):
-        # 如果按下 ESC 键，则退出截图
+        # 如果按下 ESC 键，则退出
         if event.key() == Qt.Key_Escape:
             self.close()
 
@@ -64,13 +74,11 @@ class TooltipListWidget(QtWidgets.QListWidget):
         # 添加数据到窗口
         for item in final_data:
             window.add_item_with_tooltip(item)
-
         # 显示窗口
         window.show()
         window.setFocus()
         window.raise_()
         window.activateWindow()
-
         # 运行应用
         app.exec_()
 
@@ -79,19 +87,15 @@ if __name__ == "__main__":
     # 创建应用和窗口
     app = QtWidgets.QApplication(sys.argv)
     window = TooltipListWidget()
-
     # 示例数据
-    data = ["这是一个很\n长的文本，需要用工具提示来完整显示。", "另一个文本示例。"]
-
+    data = []
     # 添加数据到窗口
     for item in data:
         window.add_item_with_tooltip(item)
-
     # 显示窗口
     window.show()
     window.setFocus()
     window.raise_()
     window.activateWindow()
-
     # 运行应用
     sys.exit(app.exec_())
