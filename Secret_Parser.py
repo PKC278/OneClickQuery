e4f1a651d4656436899e2fabb8d06062318b1247
PKC278 <973134713@qq.com>
2023-12-16 16:25:23 +0800

    优化

--- Secret_Parser.py
+++ Secret_Parser.py
@@ -1,24 +1,34 @@
 import os
 
+
+def replace_keys_in_file(filename, replacements):
+    with open(filename, "r", encoding="utf-8") as f:
+        config = f.read()
+
+    for old, new in replacements.items():
+        config = config.replace(old, new)
+
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(config)
+
+
 api_key = os.environ.get("API_KEY")
 baiduocr_api_key = os.environ.get("BAIDUOCR_API_KEY")
 baiduocr_secret_key = os.environ.get("BAIDUOCR_SECRET_KEY")
 tk_token = os.environ.get("TK_TOKEN")
 
-with open("main/main.py", "r", encoding="utf-8") as f:
-    config = f.read()
-    config = config.replace('api_key = ""', f'api_key = "{api_key}"')
-    config = config.replace('tk_token = ""', f'tk_token = "{tk_token}"')
+replace_keys_in_file(
+    "main/main.py",
+    {
+        'api_key = ""': f'api_key = "{api_key}"',
+        'tk_token = ""': f'tk_token = "{tk_token}"',
+    },
+)
 
-with open("main/main.py", "w", encoding="utf-8") as f:
-    f.write(config)
-    f.close()
-
-with open("main/BaiduOCR.py", "r", encoding="utf-8") as f:
-    config = f.read()
-    config = config.replace('API_KEY = ""', f'API_KEY = "{baiduocr_api_key}"')
-    config = config.replace('SECRET_KEY = ""', f'SECRET_KEY = "{baiduocr_secret_key}"')
-
-with open("main/BaiduOCR.py", "w", encoding="utf-8") as f:
-    f.write(config)
-    f.close()
+replace_keys_in_file(
+    "main/BaiduOCR.py",
+    {
+        'API_KEY = ""': f'API_KEY = "{baiduocr_api_key}"',
+        'SECRET_KEY = ""': f'SECRET_KEY = "{baiduocr_secret_key}"',
+    },
+)
